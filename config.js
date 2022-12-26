af6ce5ce909637e426dfad171860440e2a5d7961
FireHelper <79629118+DatIT-026@users.noreply.github.com>
2022-12-26 23:59:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     introTitle: 'Hế lô cậu!',
-    introDesc: `Tớ có điều thứ này muốn cho cậu xem, nhưng cũng có điều này tớ cũng muốn hỏi cậu, cậu nhớ phải trả lời thật lòng nha`,
+    introDesc: `Tớ có thứ này muốn cho cậu xem, nhưng cũng có điều này tớ cũng muốn hỏi cậu, cậu nhớ phải trả lời thật lòng đó nha`,
     btnIntro: 'Oke nà',
     title: 'Cậu yêu tớ có phải không 😙',
     desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu yêu tớ đó nha :> ',
