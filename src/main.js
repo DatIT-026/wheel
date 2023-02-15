347d8b3a271d124d25c792d870d4129cd72e061a
DatIT-026 <tiendatha2006@gmail.com>
2023-02-15 20:57:33 +0700

    New Update2.5

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 function checkCode() {
     const code = document.getElementById("code").value;
-    const validCode = "63wA93";
+    const validCode = "63wA9f";
   
     if (code === validCode) {
       localStorage.setItem("loggedIn", true);
-      window.location = "https://firehelper.tk/playground-63wA93";
+      window.location = "https://firehelper.tk/playground-63wA9f";
     } else {
       alert("Incorrect code. Try again.");
     }
