8f9aa0d9e5ef2ccedd69c6285ab55113df697473
FadeBoyMC - Youtube <tiendatha2006@gmail.com>
2023-09-10 20:35:36 +0700

    New Update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 function checkCode() {
     const code = document.getElementById("code").value;
-    const validCode = "38AC2";
+    const validCode = "S5jLoE";
   
     if (code === validCode) {
       localStorage.setItem("loggedIn", true);
-      window.location = "./playground-38AC2";
+      window.location = "./playground-S5jLoE";
     } else {
       alert("Incorrect code. Try again.");
     }
